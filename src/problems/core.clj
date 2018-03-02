cf3e1c3979c0738ab54c971a9a0add3616d7221f
Alexandre Karpov <alexakarpov@gmail.com>
2018-03-01 20:07:28 -0500

    atoi with tests

--- src/problems/core.clj
+++ src/problems/core.clj
@@ -4,3 +4,21 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn atoi [a-seq]
+  {:pre [(coll? a-seq) ]}
+  "function to convert a list of digits to an integer number they represent"
+  (let [codes (map int a-seq)
+        valid-seq (filter #(and (>= % 48)
+                                (<= % 57))
+                          codes)
+        convert #(- (int %) 48)
+        ]
+    (if (= (count valid-seq)
+           (count codes))
+      (int (apply + (map-indexed (fn [idx value]
+                                   (let [len (count a-seq)
+                                         p10 (dec (- len idx))]
+                                     (* (Math/pow 10 p10) value)))
+                                 (map convert a-seq))))
+      :error)))
--- test/problems/core_test.clj
+++ test/problems/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [problems.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest atoi-test
+  (testing "atoi works"
+    (is (= 1234567890 (atoi [\1 \2 \3 \4 \5 \6 \7 \8 \9 \0])))
+    (is (= :error (atoi [\1 \2 \x])))
+    (is (= 42 (atoi [\0 \4 \2])))))
+
