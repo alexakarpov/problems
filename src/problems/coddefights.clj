e6ebf149727ee5ebf6f409f1ae7289d2e32a26c0
Alexandre Karpov <alexakarpov@gmail.com>
2018-06-13 20:23:05 -0400

    almost increading sequence not quite working

--- src/problems/coddefights.clj
+++ src/problems/coddefights.clj
@@ -26,3 +26,31 @@
 (adjacentElementsProduct [1 2 3 4 5 6])
 
 (apply max [1 2 3 4])
+
+
+(defn any-pairs-overlap? [ps]
+  (let [recer (fn [[b ov] v]
+                (and b (< ov v)))]
+    (reduce recer [true -1] ps)))
+
+(any-pairs-overlap? [[1 2] [3 4] [4 7]])
+
+(defn almostIncreasingSequence [s]
+  (let [ps (partition 2 1 s)
+        _ (println ps)
+        cleared (filter #(< (first %) (second %)) ps)
+        _ (println cleared)]
+    (or (= (count cleared) (count ps))
+        (and
+         (< (- (count ps) (count cleared)) 2)
+         
+         ;; after clearing the dip itself, what about the climbing back up steps?
+         ))))
+
+
+(assert (= false
+           (almostIncreasingSequence [1 3 2 1])))
+
+(assert (= false (almostIncreasingSequence [1 2 1 2])))
+
+(assert (= false (almostIncreasingSequence [1, 2, 3, 4, 5, 3, 5, 6])))
